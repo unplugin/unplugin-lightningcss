818ddcbdfd9ce008c7a00cb893ea3e4ff5efe978
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-11-29 01:43:32 +0800

    test: replace @vue-macros/test-utils with @sxzz's

--- tests/transform.test.ts
+++ tests/transform.test.ts
@@ -1,24 +1,26 @@
 import { resolve } from 'node:path'
 import { describe } from 'vitest'
-import { rollupBuild, testFixtures } from '@vue-macros/test-utils'
+import { rollupBuild, testFixtures } from '@sxzz/test-utils'
 import css from 'rollup-plugin-css-only'
 import LightningCSS from '../src/rollup'
 
 describe('transform', async () => {
   await testFixtures(
     ['tests/fixtures/*.css'],
-    (args, id) =>
-      rollupBuild(id, [
-        LightningCSS({
-          options: {
-            minify: true,
-            targets: {
-              ie: 11,
+    async (args, id) =>
+      (
+        await rollupBuild(id, [
+          LightningCSS({
+            options: {
+              minify: true,
+              targets: {
+                ie: 11,
+              },
             },
-          },
-        }),
-        css(),
-      ]),
+          }),
+          css(),
+        ])
+      ).snapshot,
     { cwd: resolve(__dirname, '..'), promise: true },
   )
 })
